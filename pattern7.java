0f55c2e8252156753c7901d6985f147040c75f70
Meghanath <meghanath.mc24@gmail.com>
2023-10-30 17:58:45 +0530

    changes done

--- pattern7.java
+++ pattern7.java
@@ -1,31 +1,28 @@
+/*
+ *****
+  ****
+   ***
+    **
+     *
+
+ */
 public class pattern7 {
     public static void main(String[] args) {
         int n=5;
-        for(int i=1;i<2*n;i++){
-            int noOfSpaces=n-i;
-            for(int s=0;s<noOfSpaces;s++){
-                System.out.print(" ");
-            }
-            if((i>=1 && i<=n)){
-                for(int j=1;j<=i;j++){
-                    System.out.print("* ");
-                }
-                System.out.println();
-        }
+        for(int i=1;i<=n;i++){
+                //to print spaces
 
-
-        else{
-            int noOfSpaces2=i-n;
-                for(int s1=0;s1<noOfSpaces2;s1++){
+                for(int k=1;k<i;k++){
                     System.out.print(" ");
-                    
                 }
-            for(int j=1;j<=(2*n)-i;j++){
-                
-                System.out.print("* ");
+                for(int j=i;j<=n;j++){
+                System.out.print("*");
+
             }
             System.out.println();
+
+        }
+        
     }
-}
-    }
+    
 }
---
+++ pattern9.java
@@ -0,0 +1,17 @@
+public class pattern9 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=2*n-i;j++){
+                System.out.print("*");
+        }
+        System.out.println();
+            for(int s=0;s<i;s++){
+                System.out.print(" ");
+            }
+            
+        
+    }
+    }
+    
+}
