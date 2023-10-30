ca65a1635e9d5c21e13198be3bd3a166f7ea7cb9
Meghanath <meghanath.mc24@gmail.com>
2023-10-30 19:54:28 +0530

    change

--- pattern12.java
+++ pattern12.java
@@ -2,19 +2,18 @@ public class pattern12 {
     public static void main(String[] args) {
         int n=5;
         for(int i=1;i<=n;i++){
-            if(i>=1 && i<=5){
-                //to print 1st set of spaces
+           //to print 1st  set of spaces
                  for(int j=1;j<i;j++){
                 System.out.print(" ");
             }
             for(int k=i;k<=n;k++){
-                System.out.println("* ");
+                System.out.print("* ");
+
+            }System.out.println();
 
             }
-
-            }
-        }
-            else{
+        
+            
                 for (int p = 0; p < n; p++) {
                     // Print leading spaces
                     for (int m = 0; m < n - 1 - p; m++) {
@@ -29,6 +28,6 @@ public class pattern12 {
                     System.out.println(); // Move to the next line
                 }
             }
-        }
-            }
+  }
+            
 
